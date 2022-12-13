986a84ecb9341a1de42b4ec96112a68b56cb231a
aakashtanwar1 <aakash97a@gmail.com>
2022-12-14 01:42:51 +0530

    moving components to cheatsheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { AngularComponent } from './angular/angular.component';
-import { CssComponent } from './css/css.component';
-import { HtmlComponent } from './html/html.component';
-import { JavascriptComponent } from './javascript/javascript.component';
+import { CssComponent } from './cheatsheet/css/css.component';
+import { HtmlComponent } from './cheatsheet/html/html.component';
+import { JavascriptComponent } from './cheatsheet/javascript/javascript.component';
 
 const routes: Routes = [
   // { path: 'html', component: HtmlComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HtmlComponent } from './html/html.component';
-import { CssComponent } from './css/css.component';
-import { JavascriptComponent } from './javascript/javascript.component';
+import { HtmlComponent } from './cheatsheet/html/html.component';
+import { CssComponent } from './cheatsheet/css/css.component';
+import { JavascriptComponent } from './cheatsheet/javascript/javascript.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { AngularComponent } from './angular/angular.component';
-import { GitComponent } from './git/git.component';
-import { ReactComponent } from './react/react.component';
-import { ExpressComponent } from './express/express.component';
-import { NodejsComponent } from './nodejs/nodejs.component';
+import { AngularComponent } from './cheatsheet/angular/angular.component';
+import { GitComponent } from './cheatsheet/git/git.component';
+import { ReactComponent } from './cheatsheet/react/react.component';
+import { ExpressComponent } from './cheatsheet/express/express.component';
+import { NodejsComponent } from './cheatsheet/nodejs/nodejs.component';
 import { CheatsheetComponent } from './cheatsheet/cheatsheet.component';
 
 @NgModule({
